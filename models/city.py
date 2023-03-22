16095aaf6c549416794e1f8fcd7e9916ac23bcc2
Valentin <5619@holbertonstudents.com>
2023-03-22 06:44:42 -0700

    update city files add condition os.getenv

--- models/city.py
+++ models/city.py
@@ -4,10 +4,16 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, DateTime, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+import os
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     
-    __tablename__ = "cities"
-    state_id = Column(String(60),  ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "cities"
+        state_id = Column(String(60),  ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
