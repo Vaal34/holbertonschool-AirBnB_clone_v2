d935648f49002b8d9b153d119b78575a5a9f2cad
Valentin <5619@holbertonstudents.com>
2023-03-22 07:08:19 -0700

    task 2 valid when put model.storage.new in init

--- models/base_model.py
+++ models/base_model.py
@@ -46,8 +46,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
-            models.storage.new(self)
-
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -57,8 +55,9 @@ class BaseModel:
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
-
+        
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
