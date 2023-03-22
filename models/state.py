df1b76628750776ca21bf35fb659e6d76fc830e9
Valentin <5619@holbertonstudents.com>
2023-03-22 03:31:15 -0700

    6. update state

--- models/state.py
+++ models/state.py
@@ -16,7 +16,8 @@ class State(BaseModel, Base):
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
-    
-    @property
-    def cities(self):
-        return self.City.state_id
+        @property
+        def cities(self):
+            return self.City.state_id
+    else:
+        name = ""
