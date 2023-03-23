a08d3765db5bd7667afeef9ad7f0ecfb70a3e853
Valentin <5619@holbertonstudents.com>
2023-03-23 07:44:28 -0700

    5. update state and city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, DateTime, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
--- models/state.py
+++ models/state.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.base_model import Base
-from models.city import City
-from sqlalchemy import Column, DateTime, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-import os
+
 
 class State(BaseModel):
     """ State class """
